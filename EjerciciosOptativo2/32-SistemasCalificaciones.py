1cf4615449aa47d325de41b2adc494d4045d4ea5
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-04-07 05:40:02 -0700

    Ejercicio calcular nota y promedio con clases

---
+++ EjerciciosOptativo2/32-SistemasCalificaciones.py
@@ -0,0 +1,21 @@
+class Estudiante:
+    def __init__(self, nombreAlumna, notas[5]):
+        self.nombreAlumna = nombreAlumna
+        self.notas = notas[]
+        pass
+    
+    def calcularNota(self, notas):
+        if notas >= 2:
+            print(f"La alumna {nombreAlumna} ha aprobado")
+        else:
+            print(F"La alumna no ha aprobado")
+            
+    def calcularPromedio(self, promedio):
+        promedio += notas
+        print(f"El promedio de la alumna: {nombreAlumna}. Es de: {promedio}")
+        
+datos = Estudiante("Ana",[5,4,3,5,4])
+datos.calcularNota()
+datos.calcularPromedio()
+
+
