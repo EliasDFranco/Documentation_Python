730187f86b0ec2eb761af3f1dcbf74afb4f4e934
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-04-07 06:11:13 -0700

    Cambiando ejercicio de calcular notas y promedio

--- EjerciciosOptativo2/32-SistemasCalificaciones.py
+++ EjerciciosOptativo2/32-SistemasCalificaciones.py
@@ -1,20 +1,23 @@
 class Estudiante:
-    def __init__(self, nombreAlumna, notas[5]):
+    def __init__(self, nombreAlumna, notas):
         self.nombreAlumna = nombreAlumna
-        self.notas = notas[]
+        self.notas = notas
         pass
     
-    def calcularNota(self, notas):
-        if notas >= 2:
-            print(f"La alumna {nombreAlumna} ha aprobado")
+    def calcularNota(self):
+        promedio = sum(self.notas) / len(self.notas) 
+        if promedio >= 60:
+            print(f"La alumna {self.nombreAlumna} ha aprobado")
+            print("Estado: Aprobado")
         else:
-            print(F"La alumna no ha aprobado")
+            print(F"La alumna {self.nombreAlumna} no ha aprobado")
+            print("Estado: DESAPROBADO")
             
-    def calcularPromedio(self, promedio):
-        promedio += notas
-        print(f"El promedio de la alumna: {nombreAlumna}. Es de: {promedio}")
+    def calcularPromedio(self):
+        promedio = sum(self.notas) / len(self.notas) 
+        print(f"El promedio de la alumna: {self.nombreAlumna}. Es de: {promedio}")
         
-datos = Estudiante("Ana",[5,4,3,5,4])
+datos = Estudiante("Ana",[70,80,65])
 datos.calcularNota()
 datos.calcularPromedio()
 
