9fa85e8e68ee7e1f286c5f1aaac0d2415f8ac10f
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-05-04 16:45:26 -0700

    Subiendo ejemplo de propiedades

--- EjerciciosOptativo2/Segunda Etapa/Tarea2.1/Propiedades.py
+++ EjerciciosOptativo2/Segunda Etapa/Tarea2.1/Propiedades.py
@@ -1,23 +1,22 @@
-# Practica con propiedades
 class claseEjemplo:
-    def __init__(self):
+    def __init__(self, valor):
         self._atributo = valor
-        
-@property
-    def atributo(self):
-    """Getter: devuelve el valor atributo"""
-    return self._atributo
-
-@atributo.settetr
-def atributo(self, nuevoValor):
-    """Setter: modifica el valor del atributo"""
-    if nuevoValor >= 0:
-        self.atributo = nuevoValor
-    else: 
-        raise ValueError("El valor debe ser mayor o igual a 0")
     
-@atributo.deleter
-def atributo(self):
-    """Deleter: elimina el atributo"""
-    print("El atributo ha sido eliminado")
-    del self._atributo
+    @property
+    def atributo(self):
+        """Getter: devuelve el valor del atributo"""
+        return self._atributo
+
+    @atributo.setter
+    def atributo(self, nuevoValor):
+        """Setter: modifica el valor del atributo"""
+        if nuevoValor >= 0:
+            self._atributo = nuevoValor  # Aquí debe asignarse a self._atributo, no a self.atributo
+        else:
+            raise ValueError("El valor debe ser mayor o igual a 0")
+    
+    @atributo.deleter
+    def atributo(self):
+        """Deleter: elimina el atributo"""
+        print("El atributo ha sido eliminado")
+        del self._atributo
