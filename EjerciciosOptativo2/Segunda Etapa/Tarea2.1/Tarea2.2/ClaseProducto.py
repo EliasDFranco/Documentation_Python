213051dac4cf0bfe01ad74e5fc83d2e2216048ff
Elias Franco Duarte <eliasdfranco@hotmail.com>
2025-05-12 06:14:26 -0700

    Subiendo ejercicio de Producto, usando propiedades

---
+++ EjerciciosOptativo2/Segunda Etapa/Tarea2.1/Tarea2.2/ClaseProducto.py
@@ -0,0 +1,27 @@
+class Producto:
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+        
+    @property
+    def precio(self):
+        return self._precio
+    
+    @precio.setter
+    def precio(self, nuevoPrecio):
+        if nuevoPrecio > 0:
+            self._precio = nuevoPrecio
+            
+        else: 
+            raise ValueError("El precio debe de ser mayor a 0.")
+        
+    @precio.deleter
+    def precio(self):
+        print("El precio ha sido eliminado correctamente")
+        del self._precio
+        
+x = Producto("Galleta", 20)
+print(x.precio)
+x.precio = 30
+print(x.precio)
+del x.precio 
